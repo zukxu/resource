23b1024c8cacf3772fdd91fe9b2e81727072b84e
zukxu <zukxu520@163.com>
2021-01-07 09:54:09 +0800

    使用注解进行返回类封装

---
+++ resource-common/src/main/java/com/zukxu/resource/common/result/ResponseResultBody.java
@@ -0,0 +1,22 @@
+package com.zukxu.resource.common.result;
+
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.lang.annotation.*;
+
+/**
+ * <p>
+ * 全局结果封装返回注解
+ * 使用这个注解就可以返回统一的json格式,  用于类和方法上
+ * </p>
+ *
+ * @author zukxu
+ * @date 2021/1/6 0006 15:44
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Documented
+@ResponseBody
+public @interface ResponseResultBody {
+
+}
