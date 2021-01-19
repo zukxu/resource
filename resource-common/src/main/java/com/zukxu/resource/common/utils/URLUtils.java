406b3f6146e2b52d9195d06ef20048e0166d2e4a
zukxu <zukxu520@163.com>
2021-01-19 15:24:12 +0800

    style(URLUtils.java): 删除无用代码
    删除无用代码
    
    ##

--- resource-common/src/main/java/com/zukxu/resource/common/utils/URLUtils.java
+++ resource-common/src/main/java/com/zukxu/resource/common/utils/URLUtils.java
@@ -16,12 +16,6 @@ public class URLUtils {
 	private static HttpURLConnection con;
 	private static int state = -1;
 
-	public static void main(String[] args) {
-		URLUtils utils = new URLUtils();
-		int url = utils.isConnect("https://picjumbo.com/");
-		System.out.println(url);
-	}
-
 	/**
 	 * 功能：检测当前URL是否可连接或是否有效,
 	 * 描述：最多连接网络 5 次, 如果 5 次都不成功，视为该地址不可用
@@ -31,25 +25,19 @@ public class URLUtils {
 	 */
 	public synchronized int isConnect(String urlStr) {
 		int counts = 0;
-		int results = -1;
 		while (counts < 5) {
 			try {
 				url = new URL(urlStr);
 				con = (HttpURLConnection) url.openConnection();
 				state = con.getResponseCode();
 				if (state == 200) {
-					System.out.println("URL可用！");
-					results = state;
+					break;
 				}
-				break;
 			} catch (Exception ex) {
 				counts++;
-				System.out.println("URL不可用，连接第 " + counts + " 次");
-				urlStr = null;
 				continue;
 			}
 		}
-		return results;
+		return state;
 	}
-
 }
